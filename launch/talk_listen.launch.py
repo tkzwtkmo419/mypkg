34cf6b91dbdb50368a5ff0003fbf17fc8f4883cc
Takumi Takazawa <tkzwpen0419@outlook.jp>
2022-12-01 14:50:14 +0900

    Add a command

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -7,8 +7,8 @@ import launch_ros.actions
 def generate_launch_description():
  
     talker = launch_ros.actions.Node(  
-    package='mypkg',
-    executable='talker',
+       package='mypkg',
+       executable='talker',
      )
     listener = launch_ros.actions.Node(
        package='mypkg',
--- mypkg/listener.py
+++ mypkg/listener.py
@@ -4,7 +4,7 @@ from std_msgs.msg import Int16
 
 def cb(msg):
     global node
-    node.get_logger().info("Listen: %d" % msg.date)
+    node.get_logger().info("Listen: %d" % msg.data)
 
 rclpy.init()
 node = Node("listener")
